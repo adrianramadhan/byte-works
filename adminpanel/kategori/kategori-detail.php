9ba69a0340e1f12a4ff9e8f47ec43c3f5a5803a5
pemanentomat <118662112+chintafitriana@users.noreply.github.com>
2023-07-25 13:43:59 +0700

    Update kategori-detail.php

--- adminpanel/kategori/kategori-detail.php
+++ adminpanel/kategori/kategori-detail.php
@@ -1,5 +1,7 @@
+<!-- untuk memanggil file navbar.php dan menyambungkan koneksi -->
 <?php
 require "../../koneksi.php";
+require "../component/navbar.php";
 
 $id = $_GET['p'];
 
@@ -10,6 +12,8 @@ $data = mysqli_fetch_array($query);
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- section detail kategori -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +22,6 @@ $data = mysqli_fetch_array($query);
 </head>
 
 <body>
-    <?php require "../component/navbar.php" ?>
 
     <div class="container mt-5">
         <h2>Detail Kategori</h2>
@@ -26,7 +29,8 @@ $data = mysqli_fetch_array($query);
             <form action="" method="post">
                 <div>
                     <label for="kategori">Kategori</label>
-                    <input type="text" name="kategori" id="kategori" class="form-control" value="<?php echo $data['nama'] ?>">
+                    <input type="text" name="kategori" id="kategori" class="form-control"
+                        value="<?php echo $data['nama'] ?>">
                 </div>
 
                 <div class="mt-2">
@@ -35,59 +39,70 @@ $data = mysqli_fetch_array($query);
                 </div>
             </form>
 
+            <!-- fungsi pada button edit -->
             <?php
             if (isset($_POST['editBtn'])) {
                 $kategori = htmlspecialchars($_POST['kategori']);
                 if ($data['nama'] == $kategori) {
             ?>
-                    <meta http-equiv="refresh" content="0; url=kategori.php">
-                    <?php
+            <meta http-equiv="refresh" content="0; url=kategori.php">
+
+            <!-- alert apabila kategori sudah ada -->
+            <?php
                 } else {
                     $query = mysqli_query($conn, "SELECT * FROM kategori WHERE nama='$kategori'");
                     $jumlahData = mysqli_num_rows($query);
 
                     if ($jumlahData > 0) {
                     ?>
-                        <div class="alert alert-warning mt-3" role="alert">
-                            Kategori Sudah Ada
-                        </div>
-                        <?php
+            <div class="alert alert-warning mt-3" role="alert">
+                Kategori Sudah Ada
+            </div>
+
+            <!-- alert apabila kategori berhasil diupdate -->
+            <?php
                     } else {
                         $query_simpan = mysqli_query($conn, "UPDATE kategori SET nama='$kategori' WHERE id='$id'");
 
                         if ($query_simpan) {
                         ?>
-                            <div class="alert alert-succeess mt-3" role="alert">
-                                Kategori Berhasil Diupdate
-                            </div>
+            <div class="alert alert-succeess mt-3" role="alert">
+                Kategori Berhasil Diupdate
+            </div>
 
-                            <meta http-equiv="refresh" content="0; url=kategori.php">
-                    <?php
+            <meta http-equiv="refresh" content="0; url=kategori.php">
+            <?php
                         }
                     }
                 }
             }
-
+            
+            // <!-- fungsi untuk mengecek apakah ada nama kategori bernama sama -->
             if (isset($_POST['deleteBtn'])) {
                 $query_check = mysqli_query($conn, "SELECT * FROM produk WHERE kategori_id='$id'");
                 $dataCount = mysqli_num_rows($query_check);
 
                 if ($dataCount > 0) {
                     ?>
-                    <div class="alert alert-primary mt-3" role="alert">
-                        Kategori tidak bisa dihapus karena sudah digunakan di produk
-                    </div>
-                <?php
-                }
 
+            <!-- alert apabila kategori berhasil diupdate -->
+            <div class="alert alert-primary mt-3" role="alert">
+                Kategori tidak bisa dihapus karena sudah digunakan di produk
+            </div>
+            <?php
+            }
+            
+            // <!-- fungsi untuk menghapus kategori -->
                 $query_delete = mysqli_query($conn, "DELETE FROM kategori WHERE id='$id'");
                 if ($query_delete) {
                 ?>
-                    <div class="alert alert-danger mt-3">
-                        Kategori Berhasil Dihapus
-                    </div>
 
-                    <meta http-equiv="refresh" content="2; url=kategori.php">
+            <!-- alert apabila kategori berhasil dihapus -->
+            <div class="alert alert-danger mt-3">
+                Kategori Berhasil Dihapus
+            </div>
+
+            <meta http-equiv="refresh" content="2; url=kategori.php">
             <?php
                 }
             }
@@ -95,6 +110,7 @@ $data = mysqli_fetch_array($query);
         </div>
     </div>
 
+<!-- memanggil script js -->
     <script src="../../bootstrap/js/bootstrap.min.js"></script>
 </body>
 
