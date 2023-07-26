cc8ec8759bca46e811dc4ef11c18772b84254802
Arisa Restu Ambarwati <134122944+arisarestuambarwati@users.noreply.github.com>
2023-07-26 10:22:27 +0700

    Update login.js

--- adminpanel/login/login.js
+++ adminpanel/login/login.js
@@ -1,5 +1,6 @@
 function validateLogin(event) {
-    event.preventDefault();
+    event.preventDefault(); // Mencegah perilaku default dari event form submission.
+    
     // Ambil input dari form login
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -12,9 +13,11 @@ function validateLogin(event) {
     if (password.trim() == "") {
       alertContainer.innerHTML = '<div class="alert alert-danger" role="alert">Password tidak boleh kosong</div>';
     }
-  
+
+    // Memeriksa apakah username dan password cocok dengan nilai yang diharapkan.
     if (username == "admin" && password == "admin") { 
       alertContainer.innerHTML = '<div class="alert alert-success" role="alert">Login sukses</div>';
+      // Mengarahkan ke halaman setelah login sukses.
       window.location.href = "http://localhost/rinjani-aquarium/adminpanel/index.php";
     } else {
       alertContainer.innerHTML = '<div class="alert alert-danger" role="alert">Username atau password salah</div>';
