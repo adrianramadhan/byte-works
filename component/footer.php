a3e66acb2ef78b7c504f2f1bb42215e9ea47849a
Arisa Restu Ambarwati <134122944+arisarestuambarwati@users.noreply.github.com>
2023-07-26 14:30:21 +0700

    Update footer.php

--- component/footer.php
+++ component/footer.php
@@ -1,35 +1,47 @@
 <div class="container-fluid py-5 content-subscribe text-light bg-dark">
+    <!-- Bagian Konten -->
     <div class="container">
+        <!-- Judul "Temui Kami" -->
         <h5 class="text-center mb-4"><b>Temui Kami</b></h5>
+        <!-- Baris berisi ikon media sosial yang diratakan ke tengah -->
         <div class="row justify-content-center">
             <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <!-- Link menuju halaman Facebook -->
                 <a style="color: white;" href="https://www.facebook.com/"><i class="fab fa-facebook fs-2"></i></a>
             </div>
             <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <!-- Link menuju halaman Instagram -->
                 <a style="color: white;" href="https://www.instagram.com/"><i class="fab fa-instagram fs-2"></i></a>
             </div>
             <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <!-- Link menuju halaman Twitter -->
                 <a style="color: white;" href="https://twitter.com/"><i class="fab fa-twitter fs-2"></i></a>
             </div>
             <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <!-- Link menuju halaman YouTube -->
                 <a style="color: white;" href="https://www.youtube.com/"><i class="fab fa-youtube fs-2"></i></a>
             </div>
         </div>
 
-
+        <!-- Bagian Subscribe -->
         <div class="text-center mt-5 mb-0"><b>Subscribe</b></div>
+        <!-- Deskripsi tentang tujuan Subscribe -->
         <p class="text-center font-monospace">Untuk informasi tentang promo</p>
         <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
             <div class="input-group mb-3">
+                <!-- Input untuk memasukkan alamat email -->
                 <input type="text" class="form-control" placeholder="Ketikan Email">
+                <!-- Tombol untuk melakukan Subscribe -->
                 <button class="btn btn-info text-light" type="button" id="button-addon2">Subscribe</button>
             </div>
         </div>
     </div>
 </div>
 
+<!-- Bagian Footer -->
 <div style="background-color: #0F0E0E;" class="container-fluid py-3 text-light">
     <div class="container text-center">
+        <!-- Teks Hak Cipta dan Tahun -->
         <label>&copy; 2023 Byte Works</label>
         <br>
     </div>
