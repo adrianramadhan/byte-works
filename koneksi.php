60116d39734ead8a89bf4da4838ff05f1ce8c637
Arisa Restu Ambarwati <134122944+arisarestuambarwati@users.noreply.github.com>
2023-07-26 13:32:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
 <?php
+    // Membuat koneksi ke server MySQL dengan host "localhost", username "root", password "" (tidak menggunakan password), dan memilih database "byte_works"
     $conn = mysqli_connect("localhost", "root", "", "byte_works");
 
+    // Memeriksa apakah koneksi berhasil
     if (mysqli_connect_errno()) {
+        // Jika koneksi gagal, tampilkan pesan error beserta informasi kesalahan menggunakan fungsi mysqli_connect_error()
         echo "Failed to connect to MySQL : " . mysqli_connect_error();
+        // Menghentikan eksekusi script lebih lanjut dengan fungsi exit()
         exit();
     }
 ?>
